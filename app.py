99159d7a68a60d203ea1d082a1838b481c7b4920
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-02-13 00:35:46 +0530

    data

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 import altair as alt
 from PIL import Image
 
-#image = Image.open('dna-logo.jpg')
+image = Image.open('dna-logo.jpg')
 
-#st.image(image, use_column_width=True)
+st.image(image, use_column_width=True)
 st.write("""
 # DNA Nucleotide Count Web App
 This app counts the nucleotide composition of query DNA!
