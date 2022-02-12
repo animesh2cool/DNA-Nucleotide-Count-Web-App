70275752b591bf877f6a68b5d6a5a62652b6287e
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-02-13 00:59:19 +0530

    data

--- app.py
+++ app.py
@@ -41,3 +41,28 @@ X = DNA_nucleotide_count(sequence)
 X_label = list(X)
 X_values = list(X.values())
 X
+
+st.subheader('2. Print text')
+st.write('There are  ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['C']) + ' thymine (C)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['T']) + ' cytosine (T)')
+
+
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+st.write(df)
+
+
+st.subheader('4. Display Bar chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80)  # controls width of bar.
+)
+st.write(p)
